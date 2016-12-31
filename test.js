7c9f5f72ecedca2a0e1dcba6b37736a433d7006b
Arnout Kazemier <info@3rd-Eden.com>
2016-12-31 14:55:42 +0100

    [test] Add more tests

--- test.js
+++ test.js
@@ -10,6 +10,18 @@ describe('stale-state', function () {
     });
   });
 
+  describe('.prevously', function () {
+    it('does not have any previous datat stored', function () {
+      assume(stale.previously).is.a('null');
+    });
+
+    it('can set the previously set data', function () {
+      stale = new Stale({ previously: 'what' });
+
+      assume(stale.previously).equals('what');
+    });
+  });
+
   describe('#majority', function () {
     it('correctly checks if the passed in value in a majority', function () {
       assume(stale.majority(1)).is.false();
@@ -19,6 +31,95 @@ describe('stale-state', function () {
       assume(stale.majority(5)).is.true();
       assume(stale.majority(6)).is.true();
       assume(stale.majority(7)).is.true();
+    });
+  });
+
+  describe('#save', function () {
+    it('calls the commit function with the data', function (next) {
+      stale.commit(function (data) {
+        assume(data).equals('foobar');
+        next();
+      });
+
+      stale.save('foobar');
+    });
+
+    it('it saves the stored data as previously', function () {
+      stale.commit(function () {});
+      stale.save('waddup');
+
+      assume(stale.previously).equals('waddup');
+    });
+
+    it('throw if theres no commit function', function (next) {
+      try { stale.save('what'); }
+      catch (e) { next(); }
+    });
+  });
+
+  describe('#verify', function () {
+    it('calls #requested for the amount of requests', function (done) {
+      done = assume.wait(7, done);
+
+      stale.check(function (previous, currently, state) {
+        if (currently.data > previous.data) state.accept();
+        else state.decline();
+      });
+
+      stale.request(function (next) {
+        setTimeout(function () {
+          next(undefined, { data: 1 });
+          done();
+        }, 0);
+      });
+
+      stale.verify({ data: 0 }, function (err, correct) {
+        assume(correct).is.true();
+        done();
+      });
+    });
+
+    it('can decline the data', function (next) {
+      stale.check(function (previous, currently, state) {
+        if (currently.data > previous.data) state.accept();
+        else state.decline();
+      });
+
+      stale.request(function (next) {
+        setTimeout(function () {
+          next(undefined, { data: 0 });
+        }, 0);
+      });
+
+      stale.verify({ data: 0 }, function (err, correct) {
+        assume(correct).is.false();
+        next();
+      });
+    });
+  });
+
+  describe('#compare', function () {
+    describe('.accept', function () {
+      it('saves the data', function (next) {
+        stale.previously = { value: 8 };
+
+        stale.commit(function (data) {
+          assume(data.value).equals(9);
+          next();
+        });
+
+        stale.check(function (previously, current, state) {
+          if (previously.value < current.value) state.accept();
+          else state.decline();
+        });
+
+        stale.compare({ value: 9 });
+      });
+    });
+
+    describe('.decline', function () {
+      it('starts the verify process');
+      it('stores the data when verify process results in allowed');
     });
   });
 
